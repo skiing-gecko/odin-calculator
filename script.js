99b6e66e1e1ad0ba875b3cded99d2a73a8dced16
skiing-gecko <115703868+skiing-gecko@users.noreply.github.com>
2025-12-13 22:04:08 +0000

    basic initial calculator functions

---
+++ script.js
@@ -0,0 +1,28 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(firstNum, secondNum, operator) {
+  switch (operator) {
+    case "+":
+      return add(firstNum, secondNum);
+    case "-":
+      return subtract(firstNum, secondNum);
+    case "*":
+      return multiply(firstNum, secondNum);
+    case  "/":
+      return divide(firstNum, secondNum);
+  }
+}
